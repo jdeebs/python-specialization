a9d60d07c523ecd4b87f61b7d8f380f1eda04047
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-24 13:29:57 -0700

    Add practice task 2

---
+++ exercise-1.3/practice-tasks/1.3-Practice Task 2.py
@@ -0,0 +1,8 @@
+test_scores = [45, 23, 89, 78, 98, 55, 74, 87, 95, 75]
+
+test_scores.sort(reverse=True)
+
+print("The first three test scores in reversed order are: ")
+
+for i in range(0, 3):
+    print(test_scores[i])
