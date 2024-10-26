4dfeb3caf57c8854ffab0057e67d5acd076b1f6b
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-26 12:17:34 -0700

    Define take recipe function to get user input for recipe info

--- exercise-1.3/Exercise_1.3.py
+++ exercise-1.3/Exercise_1.3.py
@@ -1,3 +1,28 @@
+# Initialize empty lists
 recipes_list = []
 ingredients_list = []
 
+# Define function to take a recipe
+def take_recipe():
+    print("Enter your recipe info")
+
+    # Get user input for each recipe detail
+    name = str(input("Recipe name: "))
+    cooking_time = int(input("Cooking time (in minutes): "))
+
+    # Get ingredients as a list
+    ingredients = []
+    print("Enter ingredients one by one (type 'done' to finish): ")
+    # Append each ingredient until user types done
+    while True:
+        ingredient = input("Ingredient: ")
+        if ingredient.lower() == 'done':
+            break
+        ingredients.append(ingredient)
+
+    # Recipe dictionary
+    recipe = {
+        'name': name,
+        'cooking_time': cooking_time,
+        'ingredients': ingredients
+    }
