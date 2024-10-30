0f12663d955a716e5ff15f2ab68e25196e8eee43
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-30 16:50:52 -0700

    Add date validation

--- exercise-1.5/oop.py
+++ exercise-1.5/oop.py
@@ -15,6 +15,43 @@ class Date(object):
         self.month = int(input("Enter the month: "))
         self.year = int(input("Enter the year: "))
 
+    # Simplified method to check for leap year
+    def is_leap_year(self):
+        return self.year % 4 == 0
+    
+    def is_valid_date(self):
+        # Check if values are all integers
+        if not (type(self.day) == int and type(self.month) == int and type(self.year) == int):
+            return False
+        # Check that year isn't negative
+        if self.year < 0:
+            return False
+        # Check if month is between 1-12
+        if self.month < 1 or self.month > 12:
+            return False
+        # Check if days are valid for any given month
+        # Last dates for each month in a dictionary
+        # Key = month, Value = # of days that month
+        last_dates = {
+            1: 31,
+            # Change leap year # of days to 29
+            2: 29 if self.is_leap_year() else 28,
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31,
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31
+        }
+        # Check if the day is valid for the given month
+        if self.day < 1 or self.day > last_dates.get(self.month):
+            return False
+        # If none of the above statements are triggered
+        return True
 
 # Create an instance of Date as an object
 first_moon_landing = Date(20, 7, 1969)
