6f867cb1b769a56c92bf3dc1a3363e0353b8ffa9
Jacob DiBiase <jakesback93@hotmail.com>
2024-10-30 23:36:18 -0700

    Add try-except-else block to add and remove item functions

--- exercise-1.5/practice-tasks/shopping_list.py
+++ exercise-1.5/practice-tasks/shopping_list.py
@@ -4,12 +4,26 @@ class ShoppingList(object):
         self.shopping_list = []
         
     def add_item(self, item):
-        if item not in self.shopping_list:
-            self.shopping_list.append(str(item))
+        try:
+            if item not in self.shopping_list:
+                self.shopping_list.append(str(item))
+            else:
+                raise ValueError(f"'{item}' is already in the shopping list.")
+        except:
+            print("Error adding item to shopping list.")
+        else:
+            print(f"'{item}' added to the shopping list.")
     
     def remove_item(self, item):
-        if item in self.shopping_list:
-            self.shopping_list.remove(str(item))
+        try:
+            if item in self.shopping_list:
+                self.shopping_list.remove(str(item))
+            else:
+                raise ValueError(f"'{item}' not found in the shopping list.")
+        except:
+            print("Error removing item from shopping list.")
+        else:
+            print(f"'{item}' removed from the shopping list.")
     
     def view_list(self):
         print(self.list_name)
@@ -23,7 +37,7 @@ pet_store_list.add_item("bowl")
 pet_store_list.add_item("collars")
 pet_store_list.add_item("flea collars")
 
-pet_store_list.remove_item("flea collars")
-pet_store_list.add_item("frisbee")
+pet_store_list.remove_item("flea collars") # Test removal
+pet_store_list.add_item("frisbee") # Test no duplicate items
 
 pet_store_list.view_list()
