1e6f9602f32b76490a70136b545fc1e204348212
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-12 17:05:36 -0700

    Add recipes to copy into ipython ORM

---
+++ exercise-1.7/practice-tasks/1.7-Practice Task 1.py
@@ -0,0 +1,23 @@
+tea = Recipe(
+        name = "Tea",
+        cooking_time = 5,
+        ingredients = "Tea Leaves, Water, Sugar"
+)
+
+coffee = Recipe(
+        name = "Coffee",
+        cooking_time = 5,
+        ingredients = "Coffee Powder, Sugar, Water"
+)
+
+cake = Recipe(
+        name = "Cake",
+        cooking_time = 50,
+        ingredients = "Sugar, Butter, Eggs, Vanilla Essence, Flour, Baking Powder, Milk"
+)
+
+banana_smoothie = Recipe(
+        name = "Banana Smoothie",
+        cooking_time = 5,
+        ingredients = "Bananas, Milk, Peanut Butter, Sugar, Ice Cubes"
+)
