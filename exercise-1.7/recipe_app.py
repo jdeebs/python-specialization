6e68344c1619b74ee51c9f54068bf6b410974a8f
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-19 00:27:53 -0700

    Refactor delete recipe function to handle quit correctly

--- exercise-1.7/recipe_app.py
+++ exercise-1.7/recipe_app.py
@@ -398,11 +398,13 @@ def delete_recipe():
         user_input = input("\nEnter the ID of the recipe you want to delete. Type 'quit' to exit: ")
         # Handle quit
         if user_input == 'quit':
-            break
+            return
 
         # Convert user input to integer to check ID with correct data type
         try:
             user_input = int(user_input)
+            if user_input in recipe_ids:
+                break
             # Handle if ID doesn't exist in recipe_ids
             if user_input not in recipe_ids:
                 print("The entered ID does not exist. Please try again.")
