60b75699fb89cdb3abb490a0dec9ce536f5fd72d
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-30 12:44:35 -0700

    Add Sale class

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/sales/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/sales/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# Import Book class
+from .models import Book
 
 # Create your models here.
+class Sale(models.Model):
+    # Book identifier
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    # Number of books sold
+    quantity = models.PositiveIntegerField()
+    # Total sale price for the sale
+    price = models.FloatField()
+    # Date of sale, automatically set to current date
+    date_created = models.DateTimeField(auto_now_add=True)
