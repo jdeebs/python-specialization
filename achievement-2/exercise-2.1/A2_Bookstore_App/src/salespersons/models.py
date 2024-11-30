e68a5ec744a4bba9bfaa3f8db80fc57e96f6171a
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-30 12:34:28 -0700

    Add salespersons class

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/salespersons/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/salespersons/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# Import for username attribute
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Salesperson(models.Model):
+    # OneToOne specifies each username can only be connected to one salesperson
+    # CASCADE deletes complete profile of salesperson when username is deleted
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
