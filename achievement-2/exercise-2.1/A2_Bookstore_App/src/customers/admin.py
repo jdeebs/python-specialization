0566c3ed194f340f1572f364e5e5daa1356bf98f
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-30 11:30:02 -0700

    Define model for customers

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/admin.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import Customer class
+from .models import Customer
 
 # Register your models here.
+admin.site.register(Customer)
--- achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name= models.CharField(max_length=120)
+    notes= models.TextField()
+
+    # Show customer name as string representation when queried
+    def __str__(self):
+        return str(self.name)
