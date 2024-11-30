8fb89f6cb3e7f2483b2f10544179a7ab3fae7377
Jacob DiBiase <jakesback93@hotmail.com>
2024-11-30 12:22:47 -0700

    Add Book model

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/books/admin.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import Book class
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
--- achievement-2/exercise-2.1/A2_Bookstore_App/src/books/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/books/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=120)
+    genre_choices = (
+        ('classic',
+         'Classic'),
+        ('romantic',
+         'Romantic'),
+        ('comic',
+         'Comic'),
+        ('fantasy',
+         'Fantasy'),
+        ('horror',
+         'Horror'),
+        ('educational',
+         'Educational'),
+    )
+    # List shown as dropdown to user, default value as 'classic'
+    genre = models.CharField(max_length=12, choices=genre_choices, default='classic')
+
+    book_type_choices = (
+        ('hardcover', 'Hardcover'),
+        ('ebook', 'E-Book'),
+        ('audiob', 'Audiobook'),
+    )
+    # List shown as dropdown to user, default value as 'hardcover'
+    book_type = models.CharField(max_length=12, choices=book_type_choices, default='hardcover')
+
+    # Add tooltip below form field in admin panel
+    price = models.FloatField(help_text='in US dollars $')
+    author_name = models.CharField(max_length=120)
+
+    # Show Book name as string representation when queried
+    def __str__(self):
+        return str(self.name)
