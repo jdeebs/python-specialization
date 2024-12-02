6a1768bcfc8e181b3517745c40fb2b60c22677e6
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-02 00:20:31 -0700

    Register models

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Customer(models.Model):
-    name= models.CharField(max_length=120)
-    notes= models.TextField()
+    name = models.CharField(max_length=120)
+    notes = models.TextField()
 
     # Show customer name as string representation when queried
     def __str__(self):
