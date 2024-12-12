189f81715ca82c320ab910a4a2f89b5fc97c50a1
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-12 09:47:44 -0700

    Specify url-view mapping

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/bookstore/urls.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/bookstore/urls.py
@@ -17,9 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from sales import views
+
+# Access MEDIA_URL and MEDIA_ROOT variables
+from django.conf import settings
+# Django helper func static(),
+# allows URL creation from local folder names
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sales.urls')),
 ]
+
+# Include media info in urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
