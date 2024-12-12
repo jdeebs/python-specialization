9e872d556380c752f64381bc84eb4b4181cce67b
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-12 10:04:05 -0700

    Update customer model to include pic attribute

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/customers/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=120)
     notes = models.TextField()
+    pic = models.ImageField(upload_to='customers', default='no_picture.jpg')
 
     # Show customer name as string representation when queried
     def __str__(self):
