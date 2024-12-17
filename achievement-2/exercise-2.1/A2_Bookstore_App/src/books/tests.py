aebf8fc445a48ceb675a061f1355d05e0932e7e6
Jacob DiBiase <jakesback93@hotmail.com>
2024-12-16 18:41:24 -0700

    Add test to check detail page url, fix author name max length test

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/books/tests.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/books/tests.py
@@ -30,7 +30,15 @@ class BookModelTest(TestCase):
         max_length = book._meta.get_field('author_name').max_length
 
         # Assert that the max_length is 120
-        self.assertEqual(max_length, 100)
+        self.assertEqual(max_length, 120)
 
         # Compare the authors name value to <= 120
         self.assertTrue(len(book.author_name) <= 120)
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+
+        # Should take you to the detail page of book #1
+        # and load the URL /books/list/1
+        self.assertEqual(book.get_absolute_url(), '/books/list/1')
+        
