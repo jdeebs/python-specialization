5800c8d7fb80b53cb921a2e2dcc20e62d8c51437
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-03 11:28:15 -0700

    Protect BookListView and BookDetailView CBV's

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/books/views.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/books/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 # Import to display lists and details
 from django.views.generic import ListView, DetailView
+# To protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Book
 
 # Create your views here.
-# Class Based View
-class BookListView(ListView):
+# Class Based 'protected' View
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/books.html'
 
-class BookDetailView(DetailView):
+# Class Based 'protected' View
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/detail.html'
