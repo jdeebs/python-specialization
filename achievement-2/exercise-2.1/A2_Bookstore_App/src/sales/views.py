3e2921cda69be583e9d5334d4fe6ad3a8964be1a
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-03 11:31:15 -0700

    Protect records view FBV

--- achievement-2/exercise-2.1/A2_Bookstore_App/src/sales/views.py
+++ achievement-2/exercise-2.1/A2_Bookstore_App/src/sales/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+# Protect function-based views
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'sales/home.html')
 
+# Keep protected
+@login_required
 def records(request):
     return render(request, 'sales/records.html')
